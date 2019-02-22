276547f2efde1a43161384e74c9191064ebe8e7c
Vaibhav Shukla <35499885+vaibhavshukla1@users.noreply.github.com>
2019-02-22 14:55:28 +0530

    Authentication
    Authentication

---
+++ SQL &PLSQL/Servlet/Capture_UserId_Password/src/com/capgemini/login/MyServlet.java
@@ -0,0 +1,56 @@
+package com.capgemini.login;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name=request.getParameter("username");
+		String pass=request.getParameter("password");
+		
+		if(name.equals("Vaibhav") && pass.equals("vaibhav"))
+		{
+			request.setAttribute("name", "username");
+			RequestDispatcher rd=request.getRequestDispatcher("Success");
+			rd.forward(request, response);
+			
+		}
+		else {
+			response.sendRedirect("error.html");
+		}
+		doGet(request, response);
+	}
+
+}
